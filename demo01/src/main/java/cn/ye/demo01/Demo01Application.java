dc28ab55e5d455b14496faa189f6cb56254a188b
darkYe666 <8938202482qq.com>
2017-08-23 22:37:20 +0800

    第一个函数的提交

--- demo01/src/main/java/cn/ye/demo01/Demo01Application.java
+++ demo01/src/main/java/cn/ye/demo01/Demo01Application.java
@@ -9,4 +9,9 @@ public class Demo01Application {
 	public static void main(String[] args) {
 		SpringApplication.run(Demo01Application.class, args);
 	}
+
+	//这是我的第一个提交
+    public void first(){
+        System.out.println("this is my first commit");
+    }
 }
